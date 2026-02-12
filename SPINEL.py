09951e507bcab77fadaea8bbf2292110d258f4ea
jdmchardy <90758975+jdmchardy@users.noreply.github.com>
2026-02-12 18:17:50 +0000

    Update SPINEL.py

--- SPINEL.py
+++ SPINEL.py
@@ -1251,7 +1251,8 @@ if uploaded_file is not None:
                     results_dict[hkl_label] = df
 
                     scatter = ax.scatter(psi_list, strain_33_list, color="black", s=0.2, alpha=0.1)
-                    ax.hlines(0,0,90, color="black")
+                    ax.hlines(0,0,90, color="black", lw=0.8)
+                    ax.vlines(54.7,np.min(strain_33_list), np.max(strain_33_list),color="black", ls="dashed", lw'0.8)
                     
                     #Plot the mean strain curve
                     unique_psi = np.unique(psi_list)
