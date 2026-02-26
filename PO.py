d19f6a9eae2c92f21843e457540258aad2eb869c
jdmchardy <90758975+jdmchardy@users.noreply.github.com>
2026-02-26 13:22:30 +0000

    Update PO.py

--- PO.py
+++ PO.py
@@ -1,1 +1,33 @@
+import numpy as np
 
+
+class PO_Model:
+    """
+    Preferred Orientation Model
+    Models the effects of preferred orientation on diffracted x-ray intensity
+    """
+    def __init__(self, po_model="MarchDollase",
+                 components=[{"tau": 0,  "rho": 0,  "R": 1, "weight": 1}], #default of one component direction (R=1 is isotropic = no PO) aligned with stress z-axis
+                 baseline=0, #A constant baseline value
+                ):
+        """
+        Parameters
+        ----------
+        po_model : str
+            Preferred orientation model to use.
+        components : list of dic
+            One dictionary per component direction. Each dictionary contains
+            "tau" : float (degrees)
+                  The tilt angle from the stress axis
+            "rho" : float (degrees)
+                  The rotation angle around the stress axis
+            "R" : float (Typically between 0 and 1)
+                  The March-Dollase factor
+            "weight" : float (Between 0 and 1)
+                  The relative weight of the component.
+        baseline : float
+            A constant baseline value for the intensity. Between 0 and 1
+        """
+        self.po_model = po_model
+        self.components = components
+        self.baseline = baseline
