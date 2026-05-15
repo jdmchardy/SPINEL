cc7fa6c82e7f3ddfc5531c2cd2ed4ffb2ed88786
jdmchardy <90758975+jdmchardy@users.noreply.github.com>
2026-05-15 13:00:57 +0100

    Update SPINEL.py

--- SPINEL.py
+++ SPINEL.py
@@ -501,7 +501,7 @@ def compute_strain(hkl, intensity, symmetry, lattice_params, wavelength, cij_par
         else:
             #Reduce the sampling of datapoints in phi as we must also iterate over alpha
             #This approximately preserves the number of samping points requested
-            alpha_values = np.radians(np.linspace(-180,180, 10))
+            alpha_values = np.radians(np.linspace(-180,180, 36))
             phi_samping = len(phi_values)
             new_sampling = int(phi_samping/len(alpha_values))
             phi_values = np.asarray(np.radians(np.linspace(0,360, new_sampling)))
