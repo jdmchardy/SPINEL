ed60904fe7857b602de3d9c8e957963c53fb8432
jdmchardy <90758975+jdmchardy@users.noreply.github.com>
2026-05-15 13:22:53 +0100

    Update SPINEL.py

--- SPINEL.py
+++ SPINEL.py
@@ -501,15 +501,15 @@ def compute_strain(hkl, intensity, symmetry, lattice_params, wavelength, cij_par
         else:
             psi_sampling = len(psi_values)
             #Reduce sampling of psi
-            new_psi_sampling = int(psi_sampling/2)
+            new_psi_sampling = int(psi_sampling/4)
             psi_values = np.asarray(np.radians(np.linspace(0,90, new_psi_sampling)))
             #Reduce the sampling of datapoints in phi as we must also iterate over alpha
             #This approximately preserves the number of samping points requested
             alpha_values = np.radians(np.linspace(-180,180, 36))
             phi_sampling = len(phi_values)
             new_sampling = int(phi_sampling/len(alpha_values))
-            #Double the phi_sampling to compensate for reduction in psi sampling
-            new_phi_sampling = 2*new_sampling
+            #4x the phi_sampling to compensate for reduction in psi sampling
+            new_phi_sampling = 4*new_sampling
             st.write(new_phi_sampling)
             phi_values = np.asarray(np.radians(np.linspace(0,360, new_phi_sampling)))
             #Add deltas placeholder for completeness
