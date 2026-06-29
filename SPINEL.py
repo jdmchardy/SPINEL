f5b337439124038d7dff602307e3922bdc7e3c75
James McHardy <90758975+jdmchardy@users.noreply.github.com>
2026-06-29 15:47:14 +0100

    Update SPINEL.py

--- SPINEL.py
+++ SPINEL.py
@@ -349,17 +349,25 @@ def compute_alpha(theta0, chi_rad, delta_grid_rad):
         inputs. Continuous in δ except for the arctan2 branch wrap at
         δ = ±π.
     """
+    #num = np.cos(theta0) * np.sin(delta_grid_rad)
+    #den = (
+    #    np.cos(chi_rad) * np.cos(theta0) * np.cos(delta_grid_rad)
+    #    + np.sin(chi_rad) * np.sin(theta0)
+    #)
+    #alpha = np.arctan2(num, den)
+
+    #Revised to test new derivation.
+    #den = np.cos(theta0) * np.sin(delta_grid_rad)
+    #num = (
+    #    np.sin(chi_rad) * np.sin(theta0)-np.cos(chi_rad) * np.cos(theta0) * np.cos(delta_grid_rad)
+    #)
+    #alpha = np.arctan2(num, den)
+
+    #third option
     num = np.cos(theta0) * np.sin(delta_grid_rad)
     den = (
         np.cos(chi_rad) * np.cos(theta0) * np.cos(delta_grid_rad)
-        + np.sin(chi_rad) * np.sin(theta0)
-    )
-    alpha = np.arctan2(num, den)
-
-    #Revised to test new derivation. This is not what is in the SPINEL paper!!!
-    den = np.cos(theta0) * np.sin(delta_grid_rad)
-    num = (
-        np.sin(chi_rad) * np.sin(theta0)-np.cos(chi_rad) * np.cos(theta0) * np.cos(delta_grid_rad)
+        - np.sin(chi_rad) * np.sin(theta0)
     )
     alpha = np.arctan2(num, den)
     return alpha
